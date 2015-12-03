60d25f9b45078da726640475d698d22c8a2dcb6c
Maxime Bernard <mbernard@wemanity.com>
2015-12-03 14:52:33 +0100

    Now we have what we should have.

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,12 +4,15 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../../package.json');
+  },
   prompting: function () {
     var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the ace ' + chalk.red('generator-gulp-website') + ' generator!'
+      'Welcome to the WeStud.io ' + chalk.red('generator-gulp-website') + ' generator! I include Gulp and Sass.'
     ));
 
     var prompts = [{
@@ -29,12 +32,82 @@ module.exports = yeoman.generators.Base.extend({
 
   writing: function () {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js')
     );
+    
+    this.fs.copy(
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json')
+    );
+    
+    this.fs.copy(
+      this.templatePath('gitignore'),
+      this.destinationPath('.gitignore')
+    );
+
+    this.fs.copy(
+      this.templatePath('gitattributes'),
+      this.destinationPath('.gitattributes')
+    );
+    
+    var bowerJson = {
+        name: _s.slugify(this.appname),
+        private: true,
+        dependencies: {}
+      };  
+    bowerJson.dependencies['jquery'] = '~2.1.1';
+    this.fs.writeJSON('bower.json', bowerJson);
+    this.fs.copy(
+      this.templatePath('bowerrc'),
+      this.destinationPath('.bowerrc')
+    );
+    
+    this.fs.copy(
+      this.templatePath('editorconfig'),
+      this.destinationPath('.editorconfig')
+    );
+      
+    this.fs.copy(
+      this.templatePath('favicon.ico'),
+      this.destinationPath('app/favicon.ico')
+    );
+    
+    this.fs.copy(
+      this.templatePath('robots.txt'),
+      this.destinationPath('app/robots.txt')
+    );
+    
+    this.fs.copy(
+      this.templatePath('main.scss'),
+      this.destinationPath('app/main.scss')
+    );
+    
+    this.fs.copyTpl(
+      this.templatePath('index.html'),
+      this.destinationPath('app/index.html'),
+      {
+          appname: this.appname
+      }
+    );
+    
+    mkdirp('app/images');
+    mkdirp('app/fonts');
   },
 
   install: function () {
     this.installDependencies();
+  },
+  
+  end: function() {
+    var bowerJson = this.fs.readJSON(this.destinationPath('bower.json'));
+
+    // wire Bower packages to .scss
+    wiredep({
+      bowerJson: bowerJson,
+      directory: 'bower_components',
+      ignorePath: /^(\.\.\/)+/,
+      src: 'app/styles/*.scss'
+    });
   }
 });
---
+++ generators/app/templates/gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var wrench = require('wrench');
+var modernizr = require('gulp-modernizr');
+
+
+
+var options = {
+  src: 'src',
+  dist: 'dist',
+  tmp: '.tmp',
+  e2e: 'e2e',
+  errorHandler: function(title) {
+    return function(err) {
+      gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+      this.emit('end');
+    };
+  },
+  wiredep: {
+    directory: 'bower_components'
+  }
+};
+
+wrench.readdirSyncRecursive('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file)(options);
+});
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('modernizr', function() {
+  gulp.src('./js/*.js')
+    .pipe(modernizr())
+    .pipe(gulp.dest("build/"))
+});
